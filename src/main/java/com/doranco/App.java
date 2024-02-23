f71b59fdb5b7676e2d9b2dbc8b59987e72c425c8
imane-R <imanebenamar22@gmail.com>
2024-02-23 11:48:25 +0100

    add branch imane

--- src/main/java/com/doranco/App.java
+++ src/main/java/com/doranco/App.java
@@ -1,15 +1,12 @@
 package com.doranco;
 
-
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        try (EntityManagerFactory ef = Persistence.createEntityManagerFactory("TpFinal")){
-
+public class App {
+    public static void main(String[] args) {
+        try (EntityManagerFactory ef = Persistence.createEntityManagerFactory("TpFinal")) {
+            System.out.println("EntityManagerFactory created");
         }
     }
 }
