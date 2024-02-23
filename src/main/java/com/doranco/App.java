e83001ef88d7a8c577b007d9e43eb9b91550eab4
BuntProduction <buntlicorn@gmail.com>
2024-02-23 11:33:03 +0100

    First commit

--- src/main/java/com/doranco/App.java
+++ src/main/java/com/doranco/App.java
@@ -1,14 +1,11 @@
 package com.doranco;
 
-
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        try (EntityManagerFactory ef = Persistence.createEntityManagerFactory("TpFinal")){
+public class App {
+    public static void main(String[] args) {
+        try (EntityManagerFactory ef = Persistence.createEntityManagerFactory("TpFinal")) {
 
         }
     }
